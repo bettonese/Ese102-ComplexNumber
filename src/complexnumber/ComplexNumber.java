639b068c596a88afc5347d612e53bbc791303bd4
lcgiovannetti <lucagiovannetti1997@hotmail.it>
2015-03-10 20:35:32 +0100

    static

--- src/complexnumber/ComplexNumber.java
+++ src/complexnumber/ComplexNumber.java
@@ -3,7 +3,46 @@ package complexnumber;
 public class ComplexNumber {
     private double re;
     private double im;
+    static private double initRe;
+    static private double initIm;
 
+    static public void setInitRectangular(double re, double im){
+        initRe=re;
+        initIm=im;
+    }
+    
+    static public void setInitPolar(double modulus, double argument) {
+        if(modulus < 0)
+        {
+            throw new IllegalArgumentException("Il modulo deve essere maggiore o uguale a 0");
+        }
+        initRe = modulus * Math.cos(argument);
+        initIm = modulus * Math.sin(argument);
+    }
+    
+    static public ComplexNumber add(ComplexNumber operand1, ComplexNumber operand2) {
+        ComplexNumber somma = new ComplexNumber();
+        somma.setRectangular(operand1.getRe()+operand2.getRe(), operand1.getIm() + operand2.getIm());
+        return somma;
+    }
+    
+    static public ComplexNumber sub(ComplexNumber operand1, ComplexNumber operand2) {
+        ComplexNumber diff = new ComplexNumber();
+        diff.setRectangular(operand1.getRe() - operand2.getRe(), operand1.getIm() - operand2.getIm());
+        return diff;
+    }
+    
+    static public ComplexNumber multiply(ComplexNumber operand1, ComplexNumber operand2) {
+        ComplexNumber mult = new ComplexNumber();
+        mult.setPolar(operand1.getModulus() * operand2.getModulus(), operand1.getArgument() + operand2.getArgument());
+        return mult;
+    }
+    
+    static public ComplexNumber divide(ComplexNumber operand1, ComplexNumber operand2) {
+        ComplexNumber divisione = new ComplexNumber();
+        divisione.setPolar(operand1.getModulus() / operand2.getModulus(), operand1.getArgument() - operand2.getArgument());
+        return divisione;
+    }
     
     public void setRectangular(double re, double im) { 
         this.re = re;
@@ -51,10 +90,16 @@ public class ComplexNumber {
     }
     public ComplexNumber multiply(ComplexNumber multipl){
         ComplexNumber moltiplicazione = new ComplexNumber();
+        double modulo = this.getModulus()*multipl.getModulus();
+        double argomento = this.getArgument()*multipl.getArgument();
+        moltiplicazione.setPolar(modulo, argomento);
         return moltiplicazione;     
     }
     public ComplexNumber divide(ComplexNumber div){
         ComplexNumber divisione = new ComplexNumber();
+        double modulo=this.getModulus()/ div.getModulus();
+        double argomento=this.getArgument()- div.getArgument();
+        divisione.setPolar(modulo, argomento);
         return divisione;     
     }
     
